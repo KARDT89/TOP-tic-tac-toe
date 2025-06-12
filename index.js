defebb778a005de931442ff3d790c3ffb259dbbb
dt89-linux <dt89@ggwp.com>
2025-06-12 12:46:18 +0530

    idk

--- index.js
+++ index.js
@@ -5,22 +5,99 @@ const Gameboard = (function () {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i].push(null);
+      board[i].push(Cell());
     }
   }
 
   const getBoard = () => board;
 
-  const makeMove = (number, player) => {
+  const makeMove = (number) => {
     const row = Math.floor((number - 1) / 3);
     const column = (number - 1) % 3;
-    board[row][column] = player
+    if(board[row][column].getValue() === 0){
+        board[row][column].addToken()
+    }else {
+        return
+    }
+     
   };
 
-  return { getBoard, makeMove };
+  const printBoard = () => {
+    const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+    console.log(boardWithCellValues);
+  };
+
+  return { getBoard, makeMove, printBoard };
 })();
 
+function Cell() {
+  let value = 0;
 
+  // Accept a player's token to change the value of the cell
+  const addToken = (player) => {
+    value = player;
+  };
 
+  // How we will retrieve the current value of this cell through closure
+  const getValue = () => value;
+
+  return {
+    addToken,
+    getValue
+  };
+}
+
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two"){
+    const board = Gameboard();
+
+    const players = [
+        {
+        name: playerOneName,
+        token: "X"
+        },
+        {
+        name: playerTwoName,
+        token: "Y"
+        }
+    ];
+
+    let activePlayer = players[0]
+
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players[0]
+    }
+
+    const getActivePlayer = () => activePlayer
+
+    const printNewRound = () => {
+    board.printBoard();
+    console.log(`${getActivePlayer().name}'s turn.`);
+  };
+
+  const playRound = (column) => {
+    // Drop a token for the current player
+    console.log(
+      `Dropping ${getActivePlayer().name}'s token into column ${column}...`
+    );
+    board.dropToken(column, getActivePlayer().token);
+
+    /*  This is where we would check for a winner and handle that logic,
+        such as a win message. */
+
+    // Switch player turn
+    switchPlayerTurn();
+    printNewRound();
+  };
+
+  // Initial play game message
+  printNewRound();
+
+  // For the console version, we will only use playRound, but we will need
+  // getActivePlayer for the UI version, so I'm revealing it now
+  return {
+    playRound,
+    getActivePlayer
+  };
+}
 
 
